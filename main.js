8e8335208c87d1cb2cf1ca6ecb5abe2dabd380ae
Pedro Veloso <pedrohveloso@hotmail.com>
2021-06-26 08:24:37 -0300

    ADD: Página -About-

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+/* Abre e fecha o menu quando clicar no ícone */
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/* Esconder o menu ao clicar em um dos itens */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
   })
 }
